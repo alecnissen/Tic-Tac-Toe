7858ed97571689edacd44203fefca2cf99de4a63
alecnissen <alecnissen@yahoo.com>
2023-01-01 14:18:28 -0500

    added eslintrc.json file, added basic structure for modules including gameBoard and displayController, still working on setting up modules correctly

---
+++ main.js
@@ -0,0 +1,49 @@
+// some IIFE
+// const foo = (() => { 
+//      // people array is not accessible outside the IIFE
+//      let people = ['bob', 'charlie', 'sue'];
+     
+//      const getPerson = (index) => {
+//        return people[index]
+//      }
+ 
+//      const addPerson = (name) => {
+//        people.push(name);
+//      }
+ 
+//      return { getPerson, addPerson } // this is the foo object
+//  )(); 
+
+
+
+
+
+
+
+
+const gameBoard = (() => { 
+  const gameBoardArr = []; 
+  
+  const determinePlayerTurn = {
+
+};
+const determineWinner = {
+
+  };
+  
+  return { gameBoard, gameBoardArr, determinePlayerTurn, determineWinner };
+})();   
+
+const displayController = (() => ({ displayController }))(); 
+
+function createPlayer() { 
+     return { 
+          
+     }
+}  
+
+
+
+
+
+
