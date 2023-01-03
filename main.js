e8758726c0beb9f4bdd1747f1290acd79d46117f
alecnissen <alecnissen@yahoo.com>
2023-01-02 19:03:07 -0500

    began adding logic to render the gameboard to the DOM within the displayController module, working on a function inside that module called renderBoard, trying to print the game board to the DOM

--- main.js
+++ main.js
@@ -65,27 +65,61 @@
 
 
 
-const gameBoard = (() => {
+const gameBoard = (() => {  
   // generates board
-  const board = new Array(9).fill(''); 
+  const board = ['', '', '', '', '', '', '', '','']; 
+
+  const checkForWin = (marker) => { 
+     const winConditions = /* something to store all possible wins */
+     // then check if one of those win conditions matches the board,
+     //  for the current marker?
+  };
 
   const setCell = (index, value) => { 
     // function that updates the contents of a given array index
   };
 
   const resetBoard = () => { 
+     // I don't ever return the actual board! I return
+    //  a static copy of the board instead, each time `getBoard` gets called.
     // function that resets the board to empty 
-  };
+    board = new Array(9).fill(''); 
+};
 
-  const getBoard = () => { 
-    // a function that returns a copy of the board 
-  };
+const getBoard = () => { 
+     // a function that returns a copy of the board 
+     return [...board]; 
+  };  
 
   return {
     setCell,
     resetBoard,
     getBoard,
+    checkForWin,
+    resetBoard, 
   };
 })();
+
+
+
+const displayController = (() => { 
+     renderBoard: function () { 
+      const boardContainer = document.createElement('container'); 
+
+      for (let i = 0; i < 10; i++) { 
+        const boardSquares = document.createElement('div'); 
+        boardSquares.classList.add('styling-for-board-squares'); 
+        // apply stles 
+        boardContainer.append(boardSquares); 
+      }
+
+     return { 
+          renderBoard 
+     }
+
+})() 
+
+
+
 
 
