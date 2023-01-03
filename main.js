3666fc2397670efb53e449f5ce835dc6d2f6bb7c
alecnissen <alecnissen@yahoo.com>
2023-01-03 16:07:21 -0500

    removed commented out code, use linter to clean up spacing, and made sure of proper indentation, all within the JS file

--- main.js
+++ main.js
@@ -1,139 +1,45 @@
-// some IIFE
-// const foo = (() => { 
-//      // people array is not accessible outside the IIFE
-//      let people = ['bob', 'charlie', 'sue'];
-     
-//      const getPerson = (index) => {
-//        return people[index]
-//      }
- 
-//      const addPerson = (name) => {
-//        people.push(name);
-//      }
- 
-//      return { getPerson, addPerson } // this is the foo object
-//  )(); 
-
-
-
-
-
-
-
-
-// const gameBoard = (() => { 
-//   const gameBoardArr = ['', '', '', '', '', '', '', '', '']; 
-
-// //   function () { 
-// //      for (let i = 0; i < 0; i++) { 
-// //           document.getElementById(i).textContent = gameBoardArr[i]; 
-// //      }
-// //   }
-  
-//   const determinePlayerTurn = { 
-
-// };
-//   const determineWinner = {
-
-//   }; 
-  
-//   return { gameBoardArr, determinePlayerTurn, determineWinner, renderGameBoard };
-// })();   
-
-
-// function createPlayer(name) { 
-     
-     
-//      return { 
-          
-//      }
-// }  
-
-
-// const displayController = (() => { 
-     
-//      const printWinner = { 
-
-//      } 
-
-
-     
-     
-//      return { printWinner }
-// })()
-
-
-
-
-const gameBoard = (() => {  
+const gameBoard = (() => {
   // generates board
-  const board = ['', '', '', '', '', '', '', '','']; 
+  const board = ['', '', '', '', '', '', '', '', ''];
 
-  const checkForWin = (marker) => { 
-      /* something to store all possible wins */
-     // then check if one of those win conditions matches the board,
-     //  for the current marker?
+  const checkForWin = (marker) => {
+    /* something to store all possible wins */
+    // then check if one of those win conditions matches the board,
+    //  for the current marker?
   };
 
-  const setCell = (index, value) => { 
+  const setCell = (index, value) => {
     // function that updates the contents of a given array index
   };
 
-  const resetBoard = () => { 
-     // I don't ever return the actual board! I return
+  const resetBoard = () => {
+    // I don't ever return the actual board! I return
     //  a static copy of the board instead, each time `getBoard` gets called.
-    // function that resets the board to empty 
-    board = new Array(9).fill(''); 
-};
+    // function that resets the board to empty
+    board = new Array(9).fill('');
+  };
 
-const getBoard = () => { 
-     // a function that returns a copy of the board 
-     return [...board]; 
-  };  
+  const getBoard = () => {
+    // a function that returns a copy of the board
+    return [...board];
+  };
 
   return {
     setCell,
     resetBoard,
     getBoard,
     checkForWin,
-    resetBoard, 
+    resetBoard,
   };
 })();
 
+function renderGameBoard() {
+  const boardContainer = document.querySelector('.board-container');
 
-
-// const displayController = (() => { 
-  
-//   return { 
-//     renderBoard 
-//   }
-  
-// })() 
-
-
-
-
-// function renderGameBoard () {  
-// const boardContainer = document.querySelector('.board-container'); 
-// //  document.body.append(boardContainer); 
-
-//  for (let i = 0; i < 10; i++) { 
-//    const boardSquares = document.createElement('div'); 
-//    boardSquares.classList.add('styling-for-board-squares'); 
-//    // apply stles 
-//    boardContainer.append(boardSquares); 
-//   }
-
-// }  
-
-
-function renderGameBoard () {
-  const boardContainer = document.querySelector('.board-container'); 
-  
-   for (let i = 0; i < 9; i++) {
-     const boardSquares = document.createElement('div'); 
-     boardSquares.classList.add('box');
-     boardContainer.append(boardSquares);
-    }
+  for (let i = 0; i < 9; i++) {
+    const boardSquares = document.createElement('div');
+    boardSquares.classList.add('box');
+    boardContainer.append(boardSquares);
   }
+}
 renderGameBoard();
